01543565b570d059f65cfca30e729d734c226fdc
prashant mahajan <prashantmahajan2024@gmail.com>
2026-01-15 16:31:18 +0530

    before ecoo

--- HelloDevOps.java
+++ HelloDevOps.java
@@ -8,7 +8,7 @@ public class HelloDevOps {
         HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
 
         server.createContext("/", exchange -> {
-            String response = " hiii prashant Hello hiii guys from DevOps pipeline this is done on aws and using webhook pvm";
+            String response = " hiii prashant mahajan Hello hiii guys from DevOps pipeline this is done on aws and using webhook pvm";
             exchange.sendResponseHeaders(200, response.getBytes().length);
             OutputStream os = exchange.getResponseBody();
             os.write(response.getBytes());
