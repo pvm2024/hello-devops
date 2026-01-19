501a6aa200630a182af7af831ce2e2a301c92668
prashant mahajan <prashantmahajan2024@gmail.com>
2026-01-19 15:46:45 +0530

    using domain name on prod

--- App.java
+++ App.java
@@ -6,7 +6,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
         server.createContext("/", exchange -> {
-            String response = "Hello from Prashant DevOps Production Server";
+            String response = "Hello from Prashant DevOps Production Server using domain name";
             exchange.sendResponseHeaders(200, response.length());
             try (OutputStream os = exchange.getResponseBody()) {
                 os.write(response.getBytes());
