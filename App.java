32f377c15d80fc3d94e7521f3ff6d39d51155955
prashant mahajan <prashantmahajan2024@gmail.com>
2026-01-19 14:54:39 +0530

    this is linked with http

--- App.java
+++ App.java
@@ -1,6 +1,18 @@
-public class App{
-	public static void main(String[] args){
-	System.out.println("hello from prashant DevOps pipline");
-}
+import com.sun.net.httpserver.HttpServer;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
 
+public class App {
+    public static void main(String[] args) throws Exception {
+        HttpServer server = HttpServer.create(new InetSocketAddress(9090), 0);
+        server.createContext("/", exchange -> {
+            String response = "Hello from Prashant DevOps Production Server 🚀";
+            exchange.sendResponseHeaders(200, response.length());
+            try (OutputStream os = exchange.getResponseBody()) {
+                os.write(response.getBytes());
+            }
+        });
+        server.start();
+        System.out.println("Server started on port 8080");
+    }
 }
--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
-FROM eclipse-temurin:17-jdk-jammy
+FROM eclipse-temurin:17-jdk-slim
 WORKDIR /app
 COPY . .
 RUN javac App.java
-EXPOSE 8080
+EXPOSE 9090
 CMD ["java", "App"]
