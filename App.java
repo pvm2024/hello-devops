98fe7ea9328f07ea4706e7781871b4d27e233885
prashant mahajan <prashantmahajan2024@gmail.com>
2026-01-20 18:37:51 +0530

    code change

--- App.java
+++ App.java
@@ -6,7 +6,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
         server.createContext("/", exchange -> {
-            String response = "Hello from Prashant mahajan DevOps Production Server using domain name and https with ssl& login pvm";
+            String response = "Hello from Prashant vilas  mahajan DevOps Production Server using domain name and https with ssl& login pvm";
             exchange.sendResponseHeaders(200, response.length());
             try (OutputStream os = exchange.getResponseBody()) {
                 os.write(response.getBytes());
